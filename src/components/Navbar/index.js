7f6e108628a14b3b37fb9a9d5576e500ca59a9b6
Charlie Carter <charliec1665@gmail.com>
2023-07-02 18:08:41 -0400

    uploaded wireframe

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// Bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
+
+function Navbar() {
+    return (
+        <Container>
+            <Nav>
+                
+            </Nav>
+        </Container>
+    )
+}
