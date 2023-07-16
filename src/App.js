60647dda4e00114bde443ab438907c9845fdbef3
Charlie Carter <charliec1665@gmail.com>
2023-07-16 19:15:20 -0400

    added navigation bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+// import Container from 'react-bootstrap/Container';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     // webpack and React allow us to use JSX which adds HTML to JavaScript
-    <div>
-      
-    </div>
+
+    <Navbar></Navbar>
+    
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 // Bootstrap
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function Navbar() {
+function Navigation() {
     return (
-        <Container>
-            <Nav>
-                
-            </Nav>
-        </Container>
+        <>
+        {/* May need to move Row to App.js so that the lists can be created within the second column in same row */}
+            <Row>
+                <Col xs={2} id='navbar-column'>
+                    <Navbar className='d-flex flex-column'>
+                        <h1>felici<span>task</span></h1>
+                        <Nav id='navlinks' className='d-flex flex-column'>
+                            <NavDropdown title='Channels'>
+                                {/* REMEMBER to link channels correctly later */}
+                                <NavDropdown.Item href='#all-channels'>all</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title='Work'>
+                                <NavDropdown.Item href='#all-work'>all</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title='Personal'>
+                                <NavDropdown.Item href='#all-personal'>all</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar>
+                </Col>
+            </Row>
+        </>
     )
 }
+
+export default Navigation;
